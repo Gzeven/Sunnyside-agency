c1888c2c15fa99bc261cf63dc560c4360d6d1159
Gerben van de Weerdhof <gerben1978@hotmail.com>
2023-11-09 18:09:04 +0100

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import './App.css';
-import {Navbar, Info, Testimonials, Images, Footer} from './components'
-import { BrowserRouter as Router} from 'react-router-dom'
-
+import { Navbar, Info, Testimonials, Images, Footer } from './components';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
-     <Router>
-       <Navbar/>
-       <Info/>
-       <Testimonials/>
-       <Images/>
-       <Footer/>
-     </Router>
-  
+    <Router>
+      <Navbar />
+      <Info />
+      <Testimonials />
+      <Images />
+      <Footer />
+    </Router>
   );
 }
-
-
 
 export default App;
--- src/components/Images.js
+++ src/components/Images.js
@@ -1,78 +1,69 @@
 import React from 'react';
-import styled from 'styled-components'
-import {DesktopImages, MobileImages} from '../assets'   
-    
- const Images = () => {
-    return <Wrapper>
-  <div class="container">
-  <div class="sliding-background"> 
-  </div>
-  <div class="sliding-background-reverse"></div>
-</div>
-    </Wrapper>       
-}
+import styled from 'styled-components';
+import { DesktopImages, MobileImages } from '../assets';
 
-     
-     
-
-
+const Images = () => {
+  return (
+    <Wrapper>
+      <div className="container">
+        <div className="sliding-background"></div>
+        <div className="sliding-background-reverse"></div>
+      </div>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.section`
-.container {
-  overflow: hidden;
-}
-
-.sliding-background {
-  background: url(${MobileImages}) repeat-x;
-  background-size: contain;
-  height: 188px;
-  width: 2256px;
-  animation: slide 60s linear infinite;
-  
-}
-
-.sliding-background-reverse {
-  background: url(${MobileImages}) repeat-x;
-  background-size: contain;
-  height: 188px;
-  width: 4512px;
-  animation: slide-reverse 60s linear infinite;
-  
-}
-
-@keyframes slide{
-  0% {
-    transform: translate3d(0, 0, 0);
+  .container {
+    overflow: hidden;
   }
-  100% {
-    transform: translate3d(-1504px, 0, 0);
+
+  .sliding-background {
+    background: url(${MobileImages}) repeat-x;
+    background-size: contain;
+    height: 188px;
+    width: 2256px;
+    animation: slide 60s linear infinite;
   }
-}
 
-@keyframes slide-reverse{
-  0% {
-    transform: translate3d(-1504px, 0, 0);
+  .sliding-background-reverse {
+    background: url(${MobileImages}) repeat-x;
+    background-size: contain;
+    height: 188px;
+    width: 4512px;
+    animation: slide-reverse 60s linear infinite;
   }
-  100% {
-    transform: translate3d(0, 0, 0);
+
+  @keyframes slide {
+    0% {
+      transform: translate3d(0, 0, 0);
+    }
+    100% {
+      transform: translate3d(-1504px, 0, 0);
+    }
   }
-}
 
-@media (min-width: 875px) {
-.sliding-background {
-  background: url(${DesktopImages}) repeat-x;
-  background-size: contain;
-  height: 447px;
-  width: 8640px;
-  animation: slide 60s linear infinite;
-  
-}
- .sliding-background-reverse {
-   display: none;
- }
-}
+  @keyframes slide-reverse {
+    0% {
+      transform: translate3d(-1504px, 0, 0);
+    }
+    100% {
+      transform: translate3d(0, 0, 0);
+    }
+  }
 
-
-`
+  @media (min-width: 875px) {
+    .sliding-background {
+      background: url(${DesktopImages}) repeat-x;
+      background-size: contain;
+      height: 447px;
+      width: 8640px;
+      animation: slide 60s linear infinite;
+    }
+    .sliding-background-reverse {
+      display: none;
+    }
+  }
+`;
 
 export default Images;
